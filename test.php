1a66cd8bf05db353e402527670d85f51b46cc30a
Zuza <zuzelek66@gmail.com>
2019-03-11 09:18:15 +0100

    test

--- test.php
+++ test.php
@@ -31,7 +31,14 @@ print_r($human->getAvailableClasses());
 $db = new DataBase();
 $db->createDataBase();
 
+$zuza = new Employee( 'zuza', 'zuza@op.pl', 2000, 'tester' );
+Employee::changeSallary(200);
+$extension = Employee::getExtension();
 
+$serializedExtension = serialize($extension);
+file_put_contents('extensionFile.txt', $serializedExtension);
+$unserializedExtension = unserialize(file_get_contents('extensionFile.txt'));
+var_dump($unserializedExtension);
 
 
 ?>
